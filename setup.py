4bdb836f8b844631fadb074a67f5d956bd07f5f2
Gene Hoffman <hoffmang@hoffmang.com>
2020-11-16 04:18:22 -0800

    Build python 3.9 wheels

--- setup.py
+++ setup.py
@@ -11,19 +11,20 @@ class get_pybind_include(object):
 
     The purpose of this class is to postpone importing pybind11
     until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
+    method can be invoked."""
 
     def __init__(self, user=False):
         self.user = user
 
     def __str__(self):
         import pybind11
+
         return pybind11.get_include(self.user)
 
 
 ext_modules = [
     Extension(
-        'chiabip158',
+        "chiabip158",
         [
             "src/blockfilter.cpp",
             "src/crypto/sha256.cpp",
@@ -43,7 +44,7 @@ ext_modules = [
             get_pybind_include(user=True),
             "src",
         ],
-        language='c++',
+        language="c++",
     ),
 ]
 
@@ -55,8 +56,9 @@ def has_flag(compiler, flagname):
     the specified compiler.
     """
     import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
+
+    with tempfile.NamedTemporaryFile("w", suffix=".cpp") as f:
+        f.write("int main (int argc, char **argv) { return 0; }")
         try:
             compiler.compile([f.name], extra_postargs=[flagname])
         except setuptools.distutils.errors.CompileError:
@@ -69,46 +71,43 @@ def cpp_flag(compiler):
 
     The newer version is prefered over c++11 (when it is available).
     """
-    flags = ['-std=c++17', '-std=c++14', '-std=c++11']
+    flags = ["-std=c++17", "-std=c++14", "-std=c++11"]
 
     for flag in flags:
         if has_flag(compiler, flag):
             return flag
 
-    raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                       'is needed!')
+    raise RuntimeError("Unsupported compiler -- at least C++11 support " "is needed!")
 
 
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
 
     c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
+        "msvc": ["/EHsc"],
+        "unix": [],
     }
     l_opts = {
-        'msvc': [],
-        'unix': [],
+        "msvc": [],
+        "unix": [""],
     }
 
-    if sys.platform == 'darwin':
-        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.14']
-        c_opts['unix'] += darwin_opts
-        l_opts['unix'] += darwin_opts
+    if sys.platform == "darwin":
+        darwin_opts = ["-stdlib=libc++", "-mmacosx-version-min=10.14"]
+        c_opts["unix"] += darwin_opts  # type: ignore
+        l_opts["unix"] += darwin_opts  # type: ignore
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
         opts = self.c_opts.get(ct, [])
         link_opts = self.l_opts.get(ct, [])
-        if ct == 'unix':
-            opts.append('-DVERSION_INFO="%s"'
-                        % self.distribution.get_version())
+        if ct == "unix":
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
             opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"'
-                        % self.distribution.get_version())
+            if has_flag(self.compiler, "-fvisibility=hidden"):
+                opts.append("-fvisibility=hidden")
+        elif ct == "msvc":
+            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
         for ext in self.extensions:
             ext.extra_compile_args = opts
             ext.extra_link_args = link_opts
@@ -116,17 +115,17 @@ class BuildExt(build_ext):
 
 
 setup(
-    name='chiabip158',
-    author='Mariano Sorgente',
-    author_email='mariano@chia.net',
-    description='Chia BIP158 (wraps C++)',
-    license='Apache License',
-    python_requires='>=3.7',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    setup_requires=['pybind11>=2.5.0'],
-    url='https://github.com/Chia-Network/chiabip158',
+    name="chiabip158",
+    author="Mariano Sorgente",
+    author_email="mariano@chia.net",
+    description="Chia BIP158 (wraps C++)",
+    license="Apache License",
+    python_requires=">=3.7",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    setup_requires=["pybind11>=2.5.0"],
+    url="https://github.com/Chia-Network/chiabip158",
     ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExt},
+    cmdclass={"build_ext": BuildExt},
     zip_safe=False,
 )
