4b2613befa35073551429ca44f042d2da954e3d5
Kyle Altendorf <sda@fstab.net>
2023-09-07 12:57:58 -0400

    setuptools errors again (#24)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """ based on
     https://github.com/pybind/python_example/blob/master/setup.py
 """
-from setuptools import setup, setuptools, Extension
+from setuptools import setup, errors, Extension
 from setuptools.command.build_ext import build_ext
 import sys
 
@@ -61,7 +61,7 @@ def has_flag(compiler, flagname):
         f.write("int main (int argc, char **argv) { return 0; }")
         try:
             compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
+        except errors.CompileError:
             return False
     return True
 
