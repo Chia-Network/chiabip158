63186c9454b6d9e58ba01bac5540e2d7af353a75
Rigidity <me@rigidnetwork.com>
2024-03-13 20:38:14 -0400

    oops

--- rust-bindings/src/lib.rs
+++ rust-bindings/src/lib.rs
@@ -7,7 +7,7 @@ mod bindings {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
-pub fn encode_filter(slices: &[&[u8]]) -> &'static [u8] {
+pub fn encode_filter(slices: &[&[u8]]) -> Box<[u8]> {
     let mut slices: Vec<bindings::Slice> = slices
         .iter()
         .map(|slice| bindings::Slice {
@@ -18,8 +18,7 @@ pub fn encode_filter(slices: &[&[u8]]) -> &'static [u8] {
     unsafe {
         let filter = bindings::encode_filter(slices.as_ptr(), slices.len());
         let slice = std::slice::from_raw_parts(filter.bytes, filter.length);
-        // Box::from_raw(slice as *const [u8] as *mut [u8])
-        slice
+        Box::from_raw(slice as *const [u8] as *mut [u8])
     }
 }
 
@@ -30,14 +29,4 @@ fn test_filter() {
     let elem3 = b"123";
     let filter = encode_filter(&[elem1, elem2, elem3]);
     assert_eq!(filter.as_ref(), [3, 95, 172, 194, 74, 190, 73, 221, 182]);
-}
-
-#[test]
-fn test_indefinitely() {
-    let elem1 = &[1; 10000];
-    let elem2 = &[2; 10000];
-    let elem3 = &[3; 10000];
-    loop {
-        let _filter = encode_filter(&[elem1, elem2, elem3]);
-    }
 }
