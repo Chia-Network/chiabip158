39f35d9f49c3c7e99431f97109be9b09e8d74c5a
Rigidity <me@rigidnetwork.com>
2024-03-14 20:43:15 -0400

    Fix fuzz

--- rust-bindings/fuzz/fuzz_targets/fuzz_filter.rs
+++ rust-bindings/fuzz/fuzz_targets/fuzz_filter.rs
@@ -4,6 +4,10 @@ use chiabip158::Bip158Filter;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: Vec<&[u8]>| {
+    if data.is_empty() {
+        return;
+    }
+
     let filter = Bip158Filter::new(&data);
 
     let encoded = filter.encode();
@@ -11,6 +15,8 @@ fuzz_target!(|data: Vec<&[u8]>| {
     for byte in bytes {
         let _ = *byte;
     }
+
+    assert!(filter.matches_any(&data));
 
     for elem in data {
         assert!(filter.matches(elem));
