070b427a5c97a4d049665ba86235aeeeb954689d
Rigidity <me@rigidnetwork.com>
2024-03-14 20:25:38 -0400

    Fix fuzzer? Maybe need to re-add

--- rust-bindings/fuzz/fuzz_targets/fuzz_filter.rs
+++ rust-bindings/fuzz/fuzz_targets/fuzz_filter.rs
@@ -3,8 +3,8 @@
 use chiabip158::Bip158Filter;
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: (Vec<&[u8]>, Vec<&[u8]>)| {
-    let filter = Bip158Filter::new(&data.0);
+fuzz_target!(|data: Vec<&[u8]>| {
+    let filter = Bip158Filter::new(&data);
 
     let encoded = filter.encode();
     let bytes = encoded.as_ref();
@@ -12,14 +12,7 @@ fuzz_target!(|data: (Vec<&[u8]>, Vec<&[u8]>)| {
         let _ = *byte;
     }
 
-    assert!(filter.matches_any(&data.0));
-    //assert!(!filter.matches_any(&data.1));
-
-    for elem in data.0 {
+    for elem in data {
         assert!(filter.matches(elem));
-    }
-
-    for elem in data.1 {
-        //assert!(!filter.matches(elem));
     }
 });
